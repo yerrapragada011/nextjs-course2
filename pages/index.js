927497a2bea5aea85e018632983d739cd238c700
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-16 13:44:05 -0400

    Outputting a List of Meetups

---
+++ pages/index.js
@@ -0,0 +1,26 @@
+import MeetupList from '../components/meetups/MeetupList';
+
+const DUMMY_MEETUPS = [
+  {
+    id: 'm1',
+    title: 'A First Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b5/Marburg_asv2022-02_img04_Reitgasse.jpg',
+    address: 'Some address 5, 1233 Some City',
+    description: 'This is a first meetup!',
+  },
+  {
+    id: 'm2',
+    title: 'A Second Meetup',
+    image:
+      'https://upload.wikimedia.org/wikipedia/commons/b/b5/Marburg_asv2022-02_img04_Reitgasse.jpg',
+    address: 'Some address 10, 1233 Some City',
+    description: 'This is a second meetup!',
+  },
+];
+
+function HomePage() {
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
+}
+
+export default HomePage;
