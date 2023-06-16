03ca7e408dd4ccebac5473d3da32ecb497e4a63b
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-16 16:03:05 -0400

    Adding Custom Components & CSS Modules

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '@/components/meetups/MeetupDetail';
+
+function MeetupDetails() {
+  return (
+    <MeetupDetail
+      image='https://upload.wikimedia.org/wikipedia/commons/b/b5/Marburg_asv2022-02_img04_Reitgasse.jpg'
+      title='First Meetup'
+      address='Some Street 5, Some City'
+      description='This is a first meetup'
+    />
+  );
+}
+
+export default MeetupDetails;
