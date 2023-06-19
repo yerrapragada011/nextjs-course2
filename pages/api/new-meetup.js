cc45c05625acbf14605eea37bc4890b4ce89093f
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-19 15:36:49 -0400

    Working with MongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,26 @@
+import { MongoClient } from 'mongodb';
+
 // /api/new-meetup
 // POST /api/new-meetup
 
-function handler(req, res) {
+async function handler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      'mongodb+srv://user:OOnBasIKyvugXFEL@cluster0.cgfnfcz.mongodb.net/meetups?retryWrites=true&w=majority'
+    );
+    const db = client.db();
+
+    const meetupCollection = db.collection('meetups');
+
+    const result = await meetupCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: 'Meetup inserted!' });
   }
 }
 
