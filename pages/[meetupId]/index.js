a7a446ed09e9782530b1719c46b49677bb3ea393
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-19 14:48:17 -0400

    Working with Params for SSG Data Fetching

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -11,4 +11,25 @@ function MeetupDetails() {
   );
 }
 
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  console.log(meetupId);
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          'https://upload.wikimedia.org/wikipedia/commons/b/b5/Marburg_asv2022-02_img04_Reitgasse.jpg',
+        id: meetupId,
+        title: 'First Meetup',
+        address: 'Some Street 5, Some City',
+        description: 'This is a first meetup',
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
