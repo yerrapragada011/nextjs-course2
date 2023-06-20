326d156550efa084c9f9af8122908b01177b72db
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-20 15:00:15 -0400

    Added new keyword

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -54,7 +54,7 @@ export async function getStaticProps(context) {
   const meetupsCollection = db.collection('meetups');
 
   const selectedMeetup = await meetupsCollection.findOne({
-    _id: ObjectId(meetupId),
+    _id: new ObjectId(meetupId),
   });
 
   client.close();
