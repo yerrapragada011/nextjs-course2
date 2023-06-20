f32c69b71095165188cd8cfcb1b98b86609f594b
Agasthya Yerrapragada <yerrapragada.agasthya@gmail.com>
2023-06-20 14:35:02 -0400

    Changed back to blocking from false

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -34,9 +34,9 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: 'blocking',
     paths: meetups.map((meetup) => ({
-      params: { meetupId: meetup.toString() },
+      params: { meetupId: meetup._id.toString() },
     })),
   };
 }
